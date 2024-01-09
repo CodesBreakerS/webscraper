290c8eeb67bc586c32803b42d1bac8b6538a6cf9
CodesBreakerS <monzurul.islam2022@gmail.com>
2024-01-09 21:53:37 +0600

    new project of faysl

--- pythonProject/hand.py
+++ pythonProject/hand.py
@@ -7,6 +7,7 @@ mpHands = mp.solutions.hands
 hands = mpHands.Hands()
 mpDraw = mp.solutions.drawing_utils
 while True:
+
     success, img = cap.read()
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     results = hands.process(imgRGB)
